99f235d2985954189be662bcf6b26842e0a991ad
koji-kusu <i.like.kiriboshi.daikon@gmail.com>
2021-02-22 16:02:49 +0900

    AjaxAppの準備

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,15 +1,16 @@
 class PostsController < ApplicationController
-  def index  # indexアクションを定義した
-    @posts = Post.all 
+  def index  
+    @posts = Post.all.order(id: "DESC")
   end
 
-  def new
-  end
+  
+  
 
   def create
     Post.create(content: params[:content])
+    redirect_to action: :index
   end
-  end
+  
 end
 
 
